0849ce38f3e2d846ea4247936768f6e02c824290
AmineKh14 <khamouda123@gmail.com>
2020-07-03 17:32:10 +0100

    adding core urls

--- Ebuy_Dz/urls.py
+++ Ebuy_Dz/urls.py
@@ -21,8 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth', include('allauth.urls')),
-    path('',include('core.urls')),
+    path('auth/', include('allauth.urls')),
+    #path('',include('core.urls')),
 
 
 
