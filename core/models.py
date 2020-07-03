f1f7c478ab1e418d97dd0915855bba6965ce63b9
AmineKh14 <khamouda123@gmail.com>
2020-07-03 17:47:16 +0100

    adding dedault tables

--- core/models.py
+++ core/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+
+class ProductToCart(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Cart (models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    products = models.ManyToManyField(ProductToCart)
+    date = models.DateField(auto_now_add=True)
+    
