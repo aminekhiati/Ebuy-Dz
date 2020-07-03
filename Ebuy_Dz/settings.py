18885494e2aa341802bf9510277068a93af5145b
AmineKh14 <khamouda123@gmail.com>
2020-07-03 13:09:41 +0100

    add backend auth

--- Ebuy_Dz/settings.py
+++ Ebuy_Dz/settings.py
@@ -76,13 +76,10 @@ TEMPLATES = [
 
 
 AUTHENTICATION_BACKENDS = [
-    ...
-    # Needed to login by username in Django admin, regardless of `allauth`
-    'django.contrib.auth.backends.ModelBackend',
-
-    # `allauth` specific authentication methods, such as login by e-mail
+    
+    'django.contrib.auth.backends.ModelBackend', 
     'allauth.account.auth_backends.AuthenticationBackend',
-    ...
+   
 ]
 
 
