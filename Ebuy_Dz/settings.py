577a54a8f7d734479227bcd31f616a436cd75427
AmineKh14 <khamouda123@gmail.com>
2020-07-03 13:14:18 +0100

    adding google provider

--- Ebuy_Dz/settings.py
+++ Ebuy_Dz/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
 ]
 
 SITE_ID = 1
@@ -81,6 +82,20 @@ AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend',
    
 ]
+
+
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        # For each OAuth based provider, either add a ``SocialApp``
+        # (``socialaccount`` app) containing the required client
+        # credentials, or list them here:
+        'APP': {
+            'client_id': '123',
+            'secret': '456',
+            'key': ''
+        }
+    }
+}
 
 
 WSGI_APPLICATION = 'Ebuy_Dz.wsgi.application'
