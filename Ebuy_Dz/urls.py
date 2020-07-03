dd07eba43908cb5282d57e61b5dd615478ac14b4
AmineKh14 <khamouda123@gmail.com>
2020-07-03 18:12:10 +0100

    adding home page

--- Ebuy_Dz/urls.py
+++ Ebuy_Dz/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('allauth.urls')),
-    #path('',include('core.urls')),
+    path('',include('core.urls')),
 
 
 
--- core/urls.py
+++ core/urls.py
@@ -2,3 +2,7 @@ from django.urls import path
 from . import views
 
 
+urlpatterns = [
+    path('',views.home,name='home'),
+
+] 
