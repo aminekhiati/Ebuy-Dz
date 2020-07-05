748622bd9de4a6d73e4cff9fe2efabd28a9be46a
AmineKh14 <khamouda123@gmail.com>
2020-07-05 11:51:17 +0100

    add checkout and product urls

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name='home'),
+    path('checkout',views.checkout,name='checkout'),
+    path('products',views.products,name='products'),
+
 
 ] 
