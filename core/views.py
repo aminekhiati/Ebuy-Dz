5af7231316b3b3aafe815c531a19d0100e3d17fc
AmineKh14 <khamouda123@gmail.com>
2020-07-05 11:50:16 +0100

    add products view

--- core/views.py
+++ core/views.py
@@ -8,5 +8,13 @@ from django.shortcuts import render
 def home (request):
     return render(request,'core/home-page.html')
 
+
+
 def checkout(request):
     return render(request,'core/checkout-page.html')
+
+
+
+def products(request):
+    return render(request,'core/product-page.html')
+
